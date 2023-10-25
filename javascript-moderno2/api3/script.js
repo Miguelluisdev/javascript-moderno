7fac152520f1ab2e739b598294323c0c8056a33a
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-10-24 21:45:44 -0300

    api3

--- javascript-moderno2/api3/script.js
+++ javascript-moderno2/api3/script.js
@@ -1,8 +1,56 @@
-const endpoint ="http://apiteste.miguelluis4.repl.co"
+const p_temp = document.getElementById("p_temp")
 
-let res = fetch(endpoint)
+const p_nivel = document.getElementById("p_nivel")
 
-.then(res => res.json())
-.then(dados => {
-  console.log(dados)
+const p_presss= document.getElementById("p_press")
+
+const btn_fetch = document.getElementById("btn_texto")
+
+const obterDados = () => {
+  
+  const endpoint ="http://apiteste.miguelluis4.repl.co"
+  
+  let res = fetch(endpoint)
+  
+  .then(res => res.json())
+  .then(dados => {
+    console.log(dados)
+  
+    p_temp.textContent = "remperatua:" + dados.temp
+  
+    
+    p_nivel.textContent = "nivel:" + dados.nivel
+  
+    
+    p_presss.textContent = "pressao:" + dados.pressao
+  })
+
+}
+
+// let interval = setInterval(obterDados,2000)
+
+let dados = {
+  nome: "darius", 
+  canal: "dariusmito1",
+  curso: "deus"
+
+}
+
+let cabecalho = {
+  method: "POST",
+  body: JSON.stringify(dados)
+}
+
+const gravarDados = () => {
+  const endpoint = "https://apiteste.miguelluis4.repl.co"
+
+  fetch(endpoint, cabecalho)
+  .then(res => res.json())
+  .then(ret => {
+    console.log(ret)
+  })
+}
+
+btn_fetch.addEventListener("click" , () => {
+  gravarDados()
 })
